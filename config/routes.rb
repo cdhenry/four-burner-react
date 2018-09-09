8cf92226a58bb6ee8e230509d01e2e8d9d8972b0
cdhenry <cdhenry@gmail.com>
2018-09-09 15:57:12 -0500

    chore: added basic routes using react-router-dom

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'appointments#index'
+  match '*path', to: 'appointments#index', via: :all
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
